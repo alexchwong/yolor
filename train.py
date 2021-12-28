e8cb2a0e737c9cffa39dc34df212a01144946620
alexchwong <80015046+alexchwong@users.noreply.github.com>
2021-12-28 15:42:29 +1100

    Disable final epoch plots; test for all epochs after 0

--- train.py
+++ train.py
@@ -332,7 +332,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
                 ema.update_attr(model)
             final_epoch = epoch + 1 == epochs
             if not opt.notest or final_epoch:  # Calculate mAP
-                if epoch >= 3:
+                if epoch >= 1:
                     results, maps, times = test.test(opt.data,
                                                  batch_size=batch_size*2,
                                                  imgsz=imgsz_test,
@@ -340,7 +340,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
                                                  single_cls=opt.single_cls,
                                                  dataloader=testloader,
                                                  save_dir=save_dir,
-                                                 plots=plots and final_epoch,
+                                                 plots=False, # plots and final_epoch,
                                                  log_imgs=opt.log_imgs if wandb else 0)
 
             # Write
